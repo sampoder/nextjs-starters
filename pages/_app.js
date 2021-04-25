c096dcc70324c836a38f8c9399a251d60e9f2355
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-25 13:35:33 +0800

    let there be meta!

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,101 @@
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
-import "react-virtualized/styles.css"; // only needs to be imported once
+import Head from "next/head";
+let image =
+  "https://cloud-4olahu5et-hack-club-bot.vercel.app/0nextjsstarters.png";
+let description =
+  "Browse 200+ Next.js starter templates and start using them in seconds.";
+let title = "Next.js Starters";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GeistProvider themeType={"light"}>
-      <CssBaseline />
+      <Head>
+        <meta key="og_img" property="og:image" content={image} />
+        <meta key="tw_card" name="twitter:card" content="summary_large_image" />
+        <meta key="tw_img" name="twitter:image" content={image} />
+        <meta key="desc" name="description" content={description} />
+        <meta key="og_desc" property="og:description" content={description} />
+        <meta key="tw_desc" name="twitter:description" content={description} />
+        <meta key="og_locale" property="og:locale" content="en_US" />
+        <meta key="og_type" property="og:type" content="website" />
+        <title key="title">{title}</title>
+        <meta key="og_title" property="og:title" content={title} />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
 
+      <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
   );
